82e1fca5c5528ddef038c6e6d096e4dd01e012d5
modiprabal <modiprabal@gmail.com>
2024-12-01 17:17:07 +0530

    updated config

--- modiprabal.github.io/_next/static/chunks/app/not-found-3bfcb6d26ecb4846.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{28389:function(n,e,u){Promise.resolve().then(u.t.bind(u,34724,23))}},function(n){n.O(0,[724,971,864,744],function(){return n(n.s=28389)}),_N_E=n.O()}]);
